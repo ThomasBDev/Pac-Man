dce92eaef9edab963520b2a1bf234c1d0f457821
Thomas van Deukeren <t.b.deukeren@students.uu.nl>
2018-10-22 17:50:14 +0200

    Begin Pac-Man.
    Gebruik autoexecute om het programma te starten.
    Modules zoals Main.hs staan in de src folder.
    Het programma tekent nu nog alleen een level met ASCII karakters.
    De uitkomst is wel IO(), net als de voorbeeld game, dus het is een goed voorbeeld.
    
    Modules zoals Model, View en Controller staan er nog niet in.
    (Is wel handig voor later, maar dit was nog even oefenen.)

---
+++ src/Main.hs
@@ -0,0 +1,114 @@
+module Main where
+
+main :: IO()
+main = printLevel testLevel
+
+data GameState = Title
+               | Playing
+               | Paused
+               | GameOver
+
+data Creature = Player Bool Position
+              | Ghost Bool Bool Position
+
+data Ghost = Blinky Strategy
+           | Inky   Strategy
+           | Pinky  Strategy
+           | Clyde  Strategy
+           
+data Position = Position Float Float
+
+data Strategy = Chase
+              | FlankLeft
+              | FlankRight
+              | Random
+              | Run
+              | Return
+              | Wait
+              | Leave
+              
+ghostCombo :: Int
+ghostCombo = 0
+
+lives :: Int
+lives = 3
+
+
+
+-- putStrLn neemt een String en tekent de uitkomst in de console.
+printLevel :: Level -> IO ()
+printLevel rows = putStrLn result
+                where result = concat (map printRow rows)
+                      
+printRow :: Row -> String
+printRow row = concat (map show row) ++ "\n"
+
+data Direction = North
+               | East
+               | South
+               | West
+
+data Field = C --Corridor
+           | D --Dot
+           | P --PowerDot
+           | W --Wall
+           | T --Teleporter
+           | H --Home
+
+instance Show Field where
+    show C = " "
+    show D = "."
+    show P = "+"
+    show W = "#"
+    show T = "*"
+    show H = "_"
+    
+type Level = [Row]
+type Row   = [Field]
+
+row13Walls, row5Walls, row4Walls, row9Corridors, row7Corridors, row3Corridors :: Row
+row13Walls = [W, W, W, W, W, W, W, W, W, W, W, W, W]
+row5Walls = [W, W, W, W, W]
+row4Walls = [W, W, W, W]
+row9Corridors = [C, C, C, C, C, C, C, C, C]
+row7Corridors = [C, C, C, C, C, C, C]
+row3Corridors = [C, C, C]
+row9Dots = [D, D, D, D, D, D, D, D, D]
+row7Dots = [D, D, D, D, D, D, D]
+row3Dots = [D, D, D]
+           
+testLevel, testEatenDots :: Level
+testLevel = [ row13Walls,
+              [W, P] ++ row9Dots ++ [P, W],
+              [W, C] ++ row4Walls ++ [C] ++ row4Walls ++ [C, W],
+              [W, C, W] ++ row7Corridors ++ [W, C, W],
+              [W] ++ row3Dots ++ [W, W, C, W, W] ++ row3Dots ++ [W],
+              [T, C, W, C, W, H, H, H, W, C, W, C, T],
+              [W] ++ row3Dots++ row5Walls ++ row3Dots ++ [W],
+              [W, C, W] ++ row7Corridors ++ [W, C, W],
+              [W, C] ++ row4Walls ++ [C] ++ row4Walls ++ [C, W],
+              [W, P] ++ row9Dots ++ [P, W],
+              row13Walls ]
+testEatenDots = map (map eatPacDot) testLevel
+
+              
+              
+data Item = PacDot
+          | Fruit
+          
+data PacDot = Dot
+            | PowerDot
+            
+instance Show PacDot where
+    show Dot      = "D"
+    show PowerDot = "P"
+            
+data Fruit = Float
+
+score :: Int
+score = 0
+
+eatPacDot :: Field -> Field
+eatPacDot D = C
+eatPacDot P = C
+eatPacDot x = x
